569faa0a064f779e0db75cd3ab8d30a29fe4467f
char0n9 <59369140+char0n9@users.noreply.github.com>
2020-03-25 11:22:36 +0200

    color in my console

---
+++ index.js
@@ -0,0 +1,8 @@
+const chalk = require('chalk');
+
+    console.log(chalk.blue('Ioana'));
+    console.log(chalk.yellow('Alec'));
+    console.log(chalk.cyan('Serdar'));
+    console.log(chalk.red('Andrei'));
+
+
